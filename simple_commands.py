6d9691002d2ec83c2c947cbd3f96893d99df14bd
The Vampiro Elegant <streetdemonic003@gmail.com>
2022-12-31 04:42:37 -0300

    Update simple_commands.py

--- simple_commands.py
+++ simple_commands.py
@@ -70,7 +70,7 @@ def help_handler(update: Update, context: CallbackContext):
 @user_locale
 def modes(update: Update, context: CallbackContext):
     """Handler for the /help command"""
-    modes_explanation = _("This UNO bot has four game modes: Classic, Sanic, Wild and Text.\n\n"
+    modes_explanation = _("Este bot UNO tem quatro modos de jogo: Classic, Sanic, Wild e Text.\n\n"
       " 🎻 The Classic mode uses the conventional UNO deck and there is no auto skip.\n"
       " 🚀 The Sanic mode uses the conventional UNO deck and the bot automatically skips a player if he/she takes too long to play its turn\n"
       " 🐉 The Wild mode uses a deck with more special cards, less number variety and no auto skip.\n"
