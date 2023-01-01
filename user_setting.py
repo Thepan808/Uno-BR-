ae006861df58090bf9fb879e227e836b68d7d3d8
The Vampiro Elegant <streetdemonic003@gmail.com>
2022-12-31 22:11:35 -0300

    Update user_setting.py

--- user_setting.py
+++ user_setting.py
@@ -26,7 +26,7 @@ class UserSetting(db.Entity):
 
     id = PrimaryKey(int, auto=False, size=64)  # Telegram User ID
     lang = Optional(str, default='pt_BR')  # The language setting for this user
-    stats = Optional(bool, default=False)  # Opt-in to keep game statistics
+    stats = Optional(bool, default=True)  # Opt-in to keep game statistics
     first_places = Optional(int, default=0)  # Nr. of games won in first place
     games_played = Optional(int, default=0)  # Nr. of games completed
     cards_played = Optional(int, default=0)  # Nr. of cards played total
